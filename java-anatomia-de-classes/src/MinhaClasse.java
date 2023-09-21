c04bf343559df39f3d688154e7231deb819fc121
Camila Rossi <camilarossi.contact@gmail.com>
2023-09-21 09:23:20 -0300

    anatomia de classes

---
+++ java-anatomia-de-classes/src/MinhaClasse.java
@@ -0,0 +1,10 @@
+public class MinhaClasse {
+
+public static void main (String [] args) { // inicialização
+
+    System.out.print("Hello World");
+    // criamos a classe System(classe de sistema), que vai nos dar uma saída (out), que vai ser a impressão
+
+}
+
+}
