8b0cadeeae7a5e129fbd38f1152f3dd24c3fc6ef
Camila Rossi <camilarossi.contact@gmail.com>
2023-09-24 13:31:04 -0300

    studiyng java docs

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,23 @@
+/** 
+ * <h1> Calculadora </h1>
+ * A Calculadora realiza operaçoes amtemáticas entre números inteiros
+ * 
+ * <b>Note:</b> Leia atentamente a documentação desta classe para desfrutar dos recursos oferecidos.
+ * 
+ * @author Camila Rossi
+ * @version 1.0
+ * @since 24/09/2023
+ * 
+ */
+
+public class Calculadora {
+        /**Este método é utilizado para somar dois números inteiros
+         * @param numeroUm este é o primeiro parâmento do método
+         * @param numeroDois este é o segundo parâmetro do método
+         * @return int o resultado deste método é a soma dos dois números
+         */
+    
+         public int somar(int numeroUm, int numeroDois) {
+            return numeroUm + numeroDois;
+         }
+}
