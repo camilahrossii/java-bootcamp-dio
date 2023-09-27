137e60c5cd0d5ce34a0fd2669af80593e2a09913
Camila Rossi <camilarossi.contact@gmail.com>
2023-09-27 08:46:16 -0300

    study of exception conditional

--- escopo/src/Conta.java
+++ escopo/src/Conta.java
@@ -1,5 +1,33 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Conta {
+    // variavel da classe conta
+    double saldo = 10.0;
+
+    public void sacar(Double valor){
+        //variável local do método
+        double novoSaldo = saldo - valor;
+    }
+
+    public void imprimirSaldo(){
+        // disponível em toda classe
+        System.out.println(saldo);
+        //somente o método sacar conhece essa variável
+        System.out.println(novoSaldo);
+    }
+
+    public double calcularDividaExponencial() {
+        // variável local de método
+        double valorParcela = 50.0;
+        double valorMontante = 0.0; 
+        //FOR - controle de fluxo
+        for(int x = 1; x <= 5; x++){// x variável de escopo de bloco
+            // esta variável será reiniciada a cada execução
+            double valorCalculado = valorParcela * x;
+            valorMontante = valorMontante + valorCalculado;
+        } 
+        //escopo de fluxo
+        // a unica variavel que vai estar disponivel é valorMontante para prosseguir o nosso bloco
+        // x e valorCalculado nunca estarão disponíveis fora desse bloco
+
+        return valorMontante;
     }
 }
---
+++ estruturas-excepcionais/AboutMe/src/ExemploExcecao.java
@@ -0,0 +1,15 @@
+public class ExemploExcecao {
+    public static void main(String[] args) {
+        Number valor = Double.valueOf("a1.75");
+        try{
+            valor = NumberFormat.getInstance().parse("a1.75");
+            System.out.println(valor);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        
+        
+       
+    }
+}
